f26e6887e6e0cb2e6651bf3691c867b8b2d19474
ryfazrin <riansera515@gmail.com>
2023-01-09 23:09:24 +0700

    added: state threads

---
+++ src/states/threads/action.js
@@ -0,0 +1,72 @@
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+  ADD_THREAD: 'ADD_THREAD',
+  // TOGGLE_LIKE_THREAD: 'TOGGLE_LIKE_THREAD',
+};
+
+function receiveThreadsActionCreator(threads) {
+  return {
+    type: ActionType.RECEIVE_THREADS,
+    payload: {
+      threads,
+    },
+  };
+}
+
+function addThreadActionCreator(thread) {
+  return {
+    type: ActionType.ADD_THREAD,
+    payload: {
+      thread,
+    },
+  };
+}
+
+// function toggleLikeThreadActionCreator({ threadId, userId }) {
+//   return {
+//     type: ActionType.TOGGLE_LIKE_THREAD,
+//     payload: {
+//       threadId,
+//       userId,
+//     },
+//   };
+// }
+
+function asyncAddThread({ title, body, category = '' }) {
+  return async (dispatch) => {
+    dispatch(showLoading());
+
+    try {
+      const thread = await api.createThread({ title, body, category });
+      dispatch(addThreadActionCreator(thread));
+    } catch (error) {
+      alert(error.message);
+    }
+
+    dispatch(hideLoading());
+  };
+}
+
+// function asyncToogleLikeThread(threadId) {
+//   return async (dispatch, getState) => {
+//     const { authUser } = getState();
+//     dispatch(toggleLikethreadActionCreator({ threadId, userId: authUser.id }));
+
+//     try {
+//       await api.toggleLikeThread(threadId);
+//     } catch (error) {
+//       alert(error.message);
+//       dispatch(toggleLikeThreadActionCreator({ threadId, userId: authUser.id }));
+//     }
+//   };
+// }
+
+export {
+  ActionType,
+  receiveThreadsActionCreator,
+  addThreadActionCreator,
+  asyncAddThread
+};
