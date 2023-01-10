552a422f282bfe04a48738fab50fb560eecd6e1a
ryfazrin <riansera515@gmail.com>
2023-01-10 09:22:10 +0700

    added: Redux provider and Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './states';
 // import reportWebVitals from './reportWebVitals';
 
 import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
